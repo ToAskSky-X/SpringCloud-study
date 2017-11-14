6fce3448392840b572757197781c50aa878bd72a
zhangzhiqin <pwng3hwm>
2017-11-14 21:01:49 +0800

    add

---
+++ sleuth/server-zipkin/src/main/java/org/gosky/ServerZipkinApplication.java
@@ -0,0 +1,14 @@
+package org.gosky;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author zhangzhiqin
+ * @create 2017-11-14 20:38
+ **/
+public class ServerZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerZipkinApplication.class, args);
+    }
+}
